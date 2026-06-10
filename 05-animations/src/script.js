17cc4a0d962ffbe2b1e8ae658c8caf6026edc518
Abdul Nafay <103064913+AbdulNafay42@users.noreply.github.com>
2026-06-10 22:22:35 +0500

    Add files via upload

---
+++ 05-animations/src/script.js
@@ -0,0 +1,67 @@
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Time
+// let time = Date.now()
+
+// Clock
+const clock = new THREE.Clock()
+
+// Animation
+const tick = () => {       
+
+    const elapsedTime = clock.getElapsedTime()
+    // Update objects
+    // mesh.rotation.y = elapsedTime
+    // mesh.position.y = Math.sin(elapsedTime)
+    // mesh.position.x = Math.cos(elapsedTime)
+    // mesh.position.z = Math.tan  (elapsedTime)
+    camera.position.x = Math.sin(elapsedTime)
+    camera.position.y = Math.cos(elapsedTime)
+    camera.lookAt(mesh.position)
+
+    // Time
+    // const currentTime = Date.now()
+    // const deltaTime = currentTime - time
+    // time = currentTime
+
+    // console.log(deltaTime)
+
+    // Update objects
+    // mesh.rotation.y += 0.001 * deltaTime
+
+    // Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
